da2d56377aed18c3f79b622a71dc594addba90b3
Ivan-G0nzalez <iegdlt@gmail.com>
2023-08-01 15:22:55 -0500

    Solid was applied to the code

--- email_information.py
+++ email_information.py
@@ -4,39 +4,38 @@ import pandas as pd
 import yaml
 import re
 
-pattern = r'^=\?UTF-8\?Q\?(.*?)\?='
+pattern = r'=\?UTF-8\?Q\?(.*?)\?='
 
 # Set your Gmail username and password in the credentials.yml file
 
-# open the file
-with open('credentials.yml', 'r') as f:
-    content = f.read()
+def get_credentials_from_yaml():
+    with open('credentials.yml', 'r') as f:
+        content = f.read()
 
-# load the content
-my_credentials = yaml.load(content, Loader=yaml.FullLoader)
+    my_credentials = yaml.load(content, Loader=yaml.FullLoader)
+    return my_credentials['user'], my_credentials['password']
 
-# get the username and password
-username = my_credentials['user']
-password = my_credentials['password']
+def connect_to_gmail():
+    username, password = get_credentials_from_yaml()
+    my_email = imaplib.IMAP4_SSL("imap.gmail.com")
+    my_email.login(username, password)
+    return my_email
 
-# Connect to Gmail
-my_email = imaplib.IMAP4_SSL("imap.gmail.com")
-my_email.login(username, password)
+def fetch_sender_info(email_ids, my_email):
+    sender_info = []
+    for message_id in email_ids:
+        status, msg_data = my_email.fetch(message_id, "(RFC822)")
+        if status == 'OK':
+            msg = email.message_from_bytes(msg_data[0][1])
+            data_dict = {}
+            information = email.utils.parseaddr(msg["From"])
+            sender_name = extract_sender_name(information)
+            data_dict["sender"] = sender_name[0]
+            data_dict["Email_address"] = sender_name[1]
+            sender_info.append(data_dict)
+    return sender_info
 
-# Select the "All Mail" folder
-my_email.select("Inbox")
-
-# Get a list of all emails in the folder
-status, messages = my_email.search(None, "ALL")
-message_ids = messages[0].split()
-
-num_emails_to_fetch = 100
-message_ids = message_ids[:num_emails_to_fetch]
-
-# Create a list to store the sender name and email address
-sender_info = []
-
-def correct_sender(information):
+def extract_sender_name(information):
     
     if information[0] == '':
         email = information[1]
@@ -51,36 +50,32 @@ def correct_sender(information):
 
     return information
 
-# Iterate through the list of emails
-for message_id in message_ids:
-    # Get the email
-    status, msg_data = my_email.fetch(message_id, "(RFC822)")
-    
-    # Check if the fetch was successful
-    if status == 'OK':
-        msg = email.message_from_bytes(msg_data[0][1])  # Convert bytes to email message
+def save_sender_info_to_csv(sender_info):
+    df = pd.DataFrame(sender_info)
+    df.to_csv("sender_info.csv", index=False)
 
-        # Get the sender name and email address
-        data_dict = {}
-        # This is a tupla
-        information = email.utils.parseaddr(msg["From"])
-        
-        get_information = correct_sender(information)
+def main():
+    # Connect to Gmail
+    my_email = connect_to_gmail()
 
-        sender, email_address = get_information
-        data_dict["sender"] = sender
-        data_dict["Email_address"] = email_address
+    # Select the "All Mail" folder
+    my_email.select("Inbox")
 
-        # Add the sender information to the list
-        sender_info.append(data_dict)
+    # Get a list of all emails in the folder
+    status, messages = my_email.search(None, "ALL")
+    message_ids = messages[0].split()
 
+    num_emails_to_fetch = 30
+    message_ids = message_ids[:num_emails_to_fetch]
 
+    # Fetch sender information
+    sender_info = fetch_sender_info(message_ids, my_email)
 
-# Create a Pandas DataFrame from the list
-df = pd.DataFrame(sender_info)
+    # Save the sender information to CSV
+    save_sender_info_to_csv(sender_info)
 
-# Save the DataFrame to Excel - csv
-df.to_csv("sender_info.csv", index=False)
+    # Logout from the email
+    my_email.logout()
 
-# Logout from the email
-my_email.logout()
+if __name__ == "__main__":
+    main()
